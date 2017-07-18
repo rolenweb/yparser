6f0e34f1d5416bda05ad34d729c4de2fc4b0f3e7
rolenweb <rolenweb@yandex.ru>
2017-07-18 14:04:32 +0300

    ip

--- basic/models/User.php
+++ basic/models/User.php
@@ -112,7 +112,8 @@ class User extends \yii\base\Object implements \yii\web\IdentityInterface
             '109.184.217.70',
             '89.109.23.203',
             '82.208.101.91',
-            '82.208.100.125'
+            '82.208.100.125',
+            '109.184.148.140'
         ];
     }
 }
